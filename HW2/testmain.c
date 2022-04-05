a0f18ffc4c008a1d5d83bb6fa21222f0d3426363
KCGriffith <keatongriff@gmail.com>
2022-04-05 10:32:03 -0500

    Transitioned code to proper project

--- HW2/testmain.c
+++ HW2/testmain.c
@@ -1,42 +1,36 @@
-/* 
- * File:   main.c
- * Author: hawk
- *
- * Created on April 5, 2022, 10:04 AM
- */
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
 
 // DEVCFG0
-#pragma config DEBUG = OFF // disable debugging
-#pragma config JTAGEN = OFF // disable jtag
-#pragma config ICESEL = ON // use PGED1 and PGEC1
-#pragma config PWP = OFF // disable flash write protect
-#pragma config BWP = OFF // disable boot write protect
-#pragma config CP = OFF // disable code protect
+#pragma config DEBUG = x // disable debugging
+#pragma config JTAGEN = x // disable jtag
+#pragma config ICESEL = x // use PGED1 and PGEC1
+#pragma config PWP = x // disable flash write protect
+#pragma config BWP = x // disable boot write protect
+#pragma config CP = x // disable code protect
 
 // DEVCFG1
-#pragma config FNOSC = ON // use fast frc oscillator with pll
-#pragma config FSOSCEN = OFF // disable secondary oscillator
-#pragma config IESO = OFF // disable switching clocks
-#pragma config POSCMOD = OFF // primary osc disabled
-#pragma config OSCIOFNC = OFF // disable clock output
-#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
-#pragma config FCKSM = OFF // disable clock switch and FSCM
-#pragma config WDTPS =  PS1048576 // use largest wdt value
-#pragma config WINDIS = ON // use non-window mode wdt
-#pragma config FWDTEN = OFF // wdt disabled
-#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+#pragma config FNOSC = x // use fast frc oscillator with pll
+#pragma config FSOSCEN = x // disable secondary oscillator
+#pragma config IESO = x // disable switching clocks
+#pragma config POSCMOD = x // primary osc disabled
+#pragma config OSCIOFNC = x // disable clock output
+#pragma config FPBDIV = x // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = x // disable clock switch and FSCM
+#pragma config WDTPS = x // use largest wdt value
+#pragma config WINDIS = x // use non-window mode wdt
+#pragma config FWDTEN = x // wdt disabled
+#pragma config FWDTWINSZ = x // wdt window at 25%
 
 // DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz fast rc internal oscillator
-#pragma config FPLLIDIV = DIV_4 // divide input clock to be in range 4-5MHz
-#pragma config FPLLMUL = MUL_24// multiply clock after FPLLIDIV
-#pragma config FPLLODIV = DIV_1 // divide clock after FPLLMUL to get 48MHz
+#pragma config FPLLIDIV = x // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = x // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = x // divide clock after FPLLMUL to get 48MHz
 
 // DEVCFG3
 #pragma config USERID = 0 // some 16bit userid, doesn't matter what
-#pragma config PMDL1WAY = ON // allow multiple reconfigurations
-#pragma config IOL1WAY = ON // allow multiple reconfigurations
+#pragma config PMDL1WAY = x // allow multiple reconfigurations
+#pragma config IOL1WAY = x // allow multiple reconfigurations
 
 int main() {
 
@@ -81,4 +75,3 @@ int main() {
 
     }
 }
-
